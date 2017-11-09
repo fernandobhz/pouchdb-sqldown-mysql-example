6dc8bd954bb5d24dcf144c317f1a56a685203bf6
Fernando Reis Guimaraes <fernandobhz@gmail.com>
2017-11-09 12:43:02 -0200

    table name

--- psql.js
+++ psql.js
@@ -11,13 +11,13 @@ router.get('/', async function(req, res) {
 	//SELECT ID, CONVERT(`key` USING utf8), CONVERT(value USING utf8) FROM msch.sqldown order by 1 desc
 	var myconn = 'mysql://root:Abc741963.@localhost/msch';
 	
-	var PouchDB = require('pouchdb');
+	var PouchDB = require('pouchdb').plugin(require('pouchdb-find'));
 	var sqldown = require('sqldown');
-	var SqlDownDB = PouchDB.defaults({db: function() {
-		return new sqldown(myconn);
+	var SqlDownDB = PouchDB.defaults({db: function(location) {
+		return new sqldown(myconn + '?table=' + location);
 	}});
 	
-	var db = new SqlDownDB('');
+	var db = new SqlDownDB('abc');
 
 	try { var existing = await db.get('fernando'); } catch(err) { var existing; }
 	
@@ -29,6 +29,18 @@ router.get('/', async function(req, res) {
 	await db.put(fernando);
 
 	res.json(await db.get('fernando'));
+	
+	
+	db.createIndex({
+	  index: {
+		fields: ['foo']
+	  }
+	}).then(function (result) {
+	  console.log(result);
+	}).catch(function (err) {
+	  console.log(err);
+	});
+	
 });
 
 module.exports = router;
